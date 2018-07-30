b649f6c8f76c6b2ad389df7e9a92443888d7dbc1
Ivan Borisov <2622610@gmail.com>
2018-07-30 12:47:03 +0300

    удалены

--- GLOBAL Smart/site/components/views/timer.php
+++
@@ -1,7 +0,0 @@
-<p class="token-distribution__notice">
-    <svg class="svg-icon svg-icon--danger">
-        <use xlink:href="/assets/svg-sprites/i-sprite.svg#i-danger-no-opt"></use>
-    </svg>
-
-    <?= \Yii::t("app", 'Depending on the Ethereum network traffic, figures may be delayed.') ?>
-</p>
