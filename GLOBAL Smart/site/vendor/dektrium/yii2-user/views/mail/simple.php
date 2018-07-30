610f8858146808fb77fe0277f3468a17f940bb0d
Ivan Borisov <2622610@gmail.com>
2018-07-30 12:47:32 +0300

    удалены

--- GLOBAL Smart/site/vendor/dektrium/yii2-user/views/mail/simple.php
+++
@@ -1,8 +0,0 @@
-<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0; text-align: center; font-size: 24px; text-transform: uppercase;">
-	<?= $appname ?>
-</p>
-<p>
-    <?= $message ?>
-</p>
-
-
